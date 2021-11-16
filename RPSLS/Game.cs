98c2c262f537cdba3676439367bfc4caa96c91fc
Ebony Riddick <ebonyriddick@gmail.com>
2021-11-16 17:03:03 -0500

    created files for game, humans, robots + created list for gestures + modified program file

---
+++ RPSLS/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class Game
+    {
+
+        
+        public void Run()
+        {
+            string welcome = "Welcome To RPSLS";
+            Console.WriteLine(welcome);
+
+            
+        }
+
+        string[] gestures = { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
+
+        List<string> gesturesRange = new List<string>(gestures);
+
+        public Game(string[] gestures, List<string> gesturesRange)
+        {
+            this.gestures = gestures;
+            this.gesturesRange = gesturesRange;
+        }
+    }
+}
---
+++ RPSLS/Humans.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class Humans
+    {
+        public string name;
+        public int health;
+
+
+        public Humans(string nameThatWasPassedIn, int health)
+        {
+            name = nameThatWasPassedIn;
+            this.health = health;
+        }
+
+        public Gestures(int[] number, string gesture)
+        {
+
+        }
+    }
+}
